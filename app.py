46c5aab7c06d943f05700d1802e86a8911bcb0e9
apinnya <89177094+apinnya@users.noreply.github.com>
2021-09-30 10:48:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ UPLOAD_FOLDER ='static/uploads/'
 DOWNLOAD_FOLDER = 'static/downloads/'
 ALLOWED_EXTENSIONS = {'jpg', 'png','.jpeg'}
 
-lineaccesstoken = ''
+lineaccesstoken = 'lEP+AjJRWsLgR0fdI2EkLe9Je6XC51RYC3Qgjyc5uHnzYf7v8TqkeOfMq8w66IQlGYHUZZi+oZSa2cBZnw2QsaddECrwOMVCSOnCNqco8bUCYx+mmkYhSam5791E20LoitI1P5NoRBiy7Arz5FyFZQdB04t89/1O/w1cDnyilFU='
 
 line_bot_api = LineBotApi(lineaccesstoken)
 
